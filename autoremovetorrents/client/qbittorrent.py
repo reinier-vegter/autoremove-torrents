3e1fa01983fa8902f13df462098ead2d54ea756d
Reinier V <info@reiniervegter.dev>
2023-12-07 18:00:54 +0100

    debug info

--- autoremovetorrents/client/qbittorrent.py
+++ autoremovetorrents/client/qbittorrent.py
@@ -271,7 +271,7 @@ class qBittorrent(object):
         if request.status_code != 200:
             return ([], [{
                 'hash': torrent,
-                'reason': 'The server responses HTTP %d.' % request.status_code,
+                'reason': 'The server responses HTTP %d: {0}' % (request.status_code, request),
             } for torrent in torrent_hash_list])
         # Some of them may fail but we can't judge them,
         # So we consider all of them as successful.
